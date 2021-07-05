1b82c1b56f6b139b1760bc557e3f74594261d513
Kateria Wynn <kwynn1211@gmail.com>
2021-07-05 19:52:31 -0400

    added dog images

---
+++ src/images/index.js
@@ -0,0 +1,12 @@
+import duke from '../images/duke.jpg';
+import perry from '../images/perry.jpg';
+import whiskey from '../images/whiskey.jpg';
+import tubby from '../images/tubby.jpg';
+
+const dogPics = {
+  duke,
+  perry,
+  whiskey,
+  tubby,
+};
+export default dogPics;
